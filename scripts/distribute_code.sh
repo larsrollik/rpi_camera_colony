1bc20ac5233504ea51550f81dc4744f8b8cadc44
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-05 11:23:16 +0100

    script fix for distributing code

--- scripts/distribute_code.sh
+++ scripts/distribute_code.sh
@@ -13,25 +13,10 @@ code_dir=${1:-"/home/lbr/code/rpi_camera_colony"}
 remote_code_dir=${2:-"/home/pi/code/"}
 remote_pip=${3:-"/home/pi/miniconda3/envs/py36/bin/pip"}
 remote_pip_command="$remote_pip install -e "
+repo_name=$(basename $code_dir)
 
-for remote_host in "rpi-red60" "rpi-yellow61" "rpi-blue62"  # .ssh/config host definitions
-do
-  printf "\n\n\n======================================= $remote_host \n"
+# Rsync
+parallel -j 8 rsync -av $code_dir "{1}:$remote_code_dir" --exclude=".git" --exclude="__pycache__" --exclude=".idea" --exclude="tests" --exclude="*.egg-info" ::: rpi-red60 rpi-yellow61 rpi-blue62
 
-  repo_name=$(basename $code_dir)
-  printf $code_dir $remote_code_dir $repo_name
-
-  rsync -av $code_dir "$remote_host:$remote_code_dir" \
-    --exclude=".git" \
-    --exclude="__pycache__" \
-    --exclude=".idea" \
-    --exclude="tests" \
-    --exclude="*.egg-info"
-
-
-  ssh_command="$remote_host sudo nohup ${remote_pip_command} ${remote_code_dir}/${repo_name}[rpi]"
-  printf $ssh_command
-
-  ssh $ssh_command
-
-done
+# (Re-)Install
+parallel -j 8 ssh {1} sudo nohup "${remote_pip_command} ${remote_code_dir}/${repo_name}[rpi]" ::: rpi-red60 rpi-yellow61 rpi-blue62
