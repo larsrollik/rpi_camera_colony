fdeb8f72b77e96b50f639a159c6fa67d39b907d5
Lars Rollik <l.b.rollik@protonmail.com>
2022-03-22 07:57:54 +0000

    hooks run

--- rpi_camera_colony/calibration/__main__.py
+++ rpi_camera_colony/calibration/__main__.py
@@ -23,5 +23,5 @@ def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -28,7 +28,15 @@ setup(
     author="Lars B. Rollik",
     author_email="L.B.Rollik@protonmail.com",
     license=license_text,
-    install_requires=["pyzmq", "tornado", "tqdm", "configobj", "numpy", "pandas",  "rich"]
+    install_requires=[
+        "pyzmq",
+        "tornado",
+        "tqdm",
+        "configobj",
+        "numpy",
+        "pandas",
+        "rich",
+    ]
     + rpi_install_requires,
     extras_require={
         "dev": [
